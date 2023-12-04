79f46c69728beb2ba59ed77e724a5d8c781000ff
Thieen226 <t.ho1@my.metroed.net>
2023-12-04 10:28:20 -0800

    add

---
+++ quiz.js
@@ -0,0 +1,91 @@
+var answersKey = [{
+    question: "In what country did the first Starbucks open outside of North America?",
+    answer: [
+        {"A. The U.K." : false},
+        {"B. China" : false},
+        {"C. Japan" : true},
+        {"D. Poland" : false}
+    ]
+},
+{
+    question: "Who was the highest-paid athlete in 2022?",
+    answer: [
+        {"A. Lebron James" : false},
+        {"B. Lionel Messi" : true},
+        {"C. Cristiano Ronaldo" : false},
+        {"D. Usain Bolt" : false}
+    ]
+},
+{
+    question: "What is Starbucks' logo?",
+    answer: [
+        {"A. Siren" : true},
+        {"B. Mermaid" : false},
+        {"C. Fish" : false},
+        {"D. A Woman" : false}
+    ]
+},
+{
+    question: "Who was the highest-paid athlete in 2022?",
+    answer: [
+        {"A. Lebron James" : false},
+        {"B. Lionel Messi" : true},
+        {"C. Cristiano Ronaldo" : false},
+        {"D. Usain Bolt" : false}
+    ]
+},
+{
+    question: "Who was the highest-paid athlete in 2022?",
+    answer: [
+        {"A. Lebron James" : false},
+        {"B. Lionel Messi" : true},
+        {"C. Cristiano Ronaldo" : false},
+        {"D. Usain Bolt" : false}
+    ]
+},
+{
+    question: "Who was the highest-paid athlete in 2022?",
+    answer: [
+        {"A. Lebron James" : false},
+        {"B. Lionel Messi" : true},
+        {"C. Cristiano Ronaldo" : false},
+        {"D. Usain Bolt" : false}
+    ]
+},
+{
+    question: "Who was the highest-paid athlete in 2022?",
+    answer: [
+        {"A. Lebron James" : false},
+        {"B. Lionel Messi" : true},
+        {"C. Cristiano Ronaldo" : false},
+        {"D. Usain Bolt" : false}
+    ]
+},
+{
+    question: "Who was the highest-paid athlete in 2022?",
+    answer: [
+        {"A. Lebron James" : false},
+        {"B. Lionel Messi" : true},
+        {"C. Cristiano Ronaldo" : false},
+        {"D. Usain Bolt" : false}
+    ]
+},
+{
+    question: "Who was the highest-paid athlete in 2022?",
+    answer: [
+        {"A. Lebron James" : false},
+        {"B. Lionel Messi" : true},
+        {"C. Cristiano Ronaldo" : false},
+        {"D. Usain Bolt" : false}
+    ]
+},
+{
+    question: "Who was the highest-paid athlete in 2022?",
+    answer: [
+        {"A. Lebron James" : false},
+        {"B. Lionel Messi" : true},
+        {"C. Cristiano Ronaldo" : false},
+        {"D. Usain Bolt" : false}
+    ]
+},
+] 
