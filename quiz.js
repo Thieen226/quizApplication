9775617a33591cf4d476896c597880bc87461d61
Thieen226 <t.ho1@my.metroed.net>
2023-12-08 09:39:33 -0800

    add

--- quiz.js
+++ quiz.js
@@ -26,12 +26,12 @@ var answersKey = [{
     ]
 },
 {
-    question: "Who was the highest-paid athlete in 2022?",
+    question: "According to Greek mythology, who was the first woman on earth?",
     answers: [
-        {"A. Lebron James" : false},
-        {"B. Lionel Messi" : true},
-        {"C. Cristiano Ronaldo" : false},
-        {"D. Usain Bolt" : false}
+        {"A. Helen" : false},
+        {"B. Athena" : false},
+        {"C. Pandora" : true},
+        {"D. Medusa" : false}
     ]
 },
 {
@@ -92,31 +92,62 @@ var answersKey = [{
 
 const triviaQuestion = document.getElementById("triviaQuestion");
 let currentQuestion = 0;
-//access to the question and its answers in the answersKey array 
-let questionInfo = answersKey[currentQuestion];
+let score = 0;
+let wrongAnswer = true; 
+let correctAnswer = true; 
 function loadQuestion(currentQuestion){
+    //access to the question and its answers in the answersKey array 
+    let questionInfo = answersKey[currentQuestion];
     let options = document.querySelectorAll("button");
+
     //change the text of the question to the question inside the object
     triviaQuestion.innerText = questionInfo.question;
+
     //go through answers in the question 
     for(let i = 0; i < questionInfo.answers.length; i++){
+
         //change the answer options to the answers in the answersKey array
         options[i].innerText = Object.keys(questionInfo.answers[i])[0];
-        //
+
+        //when the user click the answer options, does this
         options[i].onclick = function(){
+
+            //check if the value of answer option is true or not
             if(Object.values(questionInfo.answers[i])[0] === true){
+                wrongAnswer = true;
+                correctAnswer = true;
+
+                //change the background of the correct answer to green
                 options[i].style.backgroundColor = "#32cd32";
+
+                //if true, create a nextBtn to move to next question
                 let nextBtn = document.createElement("Button");
                 nextBtn.innerText = "Next";
                 document.body.appendChild(nextBtn);
                 nextBtn.classList.add('nextBtn');
 
+                if(correctAnswer === true && wrongAnswer === false){
+                    let points = document.getElementById("points");
+                    score += 10;
+                    points.innerText = "Points: " + score;
+                }
+
+
+                //this function will move to next question when nextBtn is clicked
                 nextBtn.onclick = function(){
-                    currentQuestion++;
-                    loadQuestion();
+                    loadQuestion(++currentQuestion);
+                    document.body.removeChild(nextBtn);
+
+                    //when move to the next question, all the answer options will change to its original color
+                    options.forEach((element) => {
+                        element.style.backgroundColor = "#F5F1CB";
+                    })
                 }
             }
             else{
+                wrongAnswer = true;
+                correctAnswer = false;
+                //change the background of the incorrect answer to red
                 options[i].style.backgroundColor = "#f44336";
             }
 
@@ -124,5 +155,5 @@ function loadQuestion(currentQuestion){
 
     }
 }
-loadQuestion();
+loadQuestion(currentQuestion);
 
