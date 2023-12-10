bc299ed48439b9ec03401ee1a2b78fe5eb878444
Thien Ho <t.ho1@my.metroed.net>
2023-12-10 00:09:47 -0800

    add

--- quiz.js
+++ quiz.js
@@ -95,7 +95,9 @@ let currentQuestion = 0;
 let score = 0; //this keeps track of correct score
 let wrongAnswerIsClicked = false; //false if the user is not clicking the wrong answer
 let correctAnswerIsClicked = false; //false if the user is not clicking the right answer
-let scoreAdded = false; //false if the score is not added
+let totalCorrectAnswer = 0; //keep track the answers that user got correct
+let totalQuestion = 0  ; //keep track the number of question
+
 function loadQuestion(currentQuestion){
     //access to the question and its answers in the answersKey array 
     let questionInfo = answersKey[currentQuestion];
@@ -134,6 +136,8 @@ function loadQuestion(currentQuestion){
                     let points = document.getElementById("points");
                     score += 10;
                     points.innerText = "Points: " + score;
+                    totalCorrectAnswer++; //add 1 to totalCorrectAnswer to know how many answers the user got correct
+                    scoreAdded = true;
                 }
                 //if the user clicks the wrong answer before the right one then add nothing
                 if(correctAnswerIsClicked === true && wrongAnswerIsClicked === true){
@@ -152,11 +156,42 @@ function loadQuestion(currentQuestion){
                         //when the nextBtn is clicked, reset the value of both correctAnswerIsClicked and wrongAnswerIsClicked
                         correctAnswerIsClicked = false;
                         wrongAnswerIsClicked = false;
+                        totalQuestion++; //add 1 when move to next question to keep track the question user do
 
                         //add 1 to the currentQuestion to move to the next question
-                        loadQuestion(++currentQuestion);
-                        document.body.removeChild(nextBtn);
+                        if(currentQuestion < answersKey.length -1){
+                            loadQuestion(++currentQuestion);
+                            document.body.removeChild(nextBtn);
 
+                        }
+                        //when the last question is answered, then let the user know they completed the quiz and their score
+                        else{
+                            triviaQuestion.innerText = "You Have Completed The Quiz!";
+                            let scoreText = document.createElement("p"); 
+                            document.body.appendChild(scoreText);
+                            triviaQuestion.appendChild(scoreText);
+                            scoreText.innerText = "You Got: " + totalCorrectAnswer + "/10";
+
+                            let quizPoint = document.createElement("p");
+                            document.body.appendChild(quizPoint);
+                            triviaQuestion.appendChild(quizPoint);
+                            quizPoint.innerText = "You Score " + score + " points";
+                           
+                            
+
+                            document.body.removeChild(nextBtn);
+
+                            //declare pointsContainer and optionsContainer
+                            let pointsContainer = document.getElementById("pointsContainer");
+                            let optionsContainer = document.getElementById("optionsContainer");
+
+                            //check if pointsContainer and optionsContainer are existed then remove both of them    
+                            if(pointsContainer && optionsContainer){
+                                pointsContainer.parentNode.removeChild(pointsContainer);
+                                optionsContainer.parentNode.removeChild(optionsContainer);
+                            }
+
+                        }
                         //when move to the next question, all the answer options will change to its original color
                         options.forEach((element) => {
                         element.style.backgroundColor = "#F5F1CB";
